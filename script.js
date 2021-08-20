3265d7ece9388a4c81516590d24d8acf023482dd
Kashish Rani <64746176+kashishrani@users.noreply.github.com>
2021-08-20 22:05:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+document.querySelector("#pencil").addEventListener("click", () => {
+    document.querySelector("#all_items").textContent = "";
+  })
+  
+  document.querySelector("#user_input").addEventListener("keydown", (event) => {
+    if(event.key == "Enter")
+      addItem();
+  });
+  
+  addItem = () => {
+    const item = document.createElement("h2")
+    item.textContent = "- " + document.querySelector("#user_input").value;
+  
+    item.addEventListener("click", () => {
+      item.style.textDecoration = "line-through";
+    })
+  
+    document.querySelector("#all_items").appendChild(item);
+    document.querySelector("#user_input").value = "";
+  }
